6245cd0bdda4dd28af12c5e028e915cf28a5455c
Rui Almeida <rui.almeida011@gmail.com>
2024-12-13 16:27:08 +0000

    wezterm config files

---
+++ .wezterm.lua
@@ -0,0 +1,60 @@
+local wezterm = require 'wezterm'
+
+local config = {
+  window_background_opacity = 0.9,
+  initial_cols = 100,
+
+  hide_tab_bar_if_only_one_tab = true,
+  -- integrated_title_buttons_style = "Gnome"
+  font = wezterm.font('Source Code Pro'),
+  font_size = 11
+}
+
+config.keys = {
+  -- This will create a new split and run your default program inside it
+  {
+    key = '-',
+    mods = 'CTRL|ALT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '+',
+    mods = 'CTRL|ALT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'w',
+    mods = 'CTRL|ALT',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+}
+
+return config
+-- config.keys =
+
+
+
+-- return {
+--     window_background_opacity = 0.9,
+--     initial_cols = 100,
+
+--     hide_tab_bar_if_only_one_tab = true,
+--     -- integrated_title_buttons_style = "Gnome"
+--     font = wezterm.font('Source Code Pro'),
+--     font_size = 11,
+
+--     keys = {
+--       {
+--         key = '+',
+--         mods = 'SHIT|ALT',
+--         action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+--       },
+--     },
+
+-- }
+-- local config = wezterm.config_builder()
+-- config.window_background_opacity = 0.90
+-- -- config.enable_tab_bar = false
+-- config.hide_tab_bar_if_only_one_tab = true
+
+-- return config
--- READMe.md
+++ READMe.md
@@ -2,10 +2,17 @@
 
 ## Files:
 
-### 99-rocal.rules
+### 1 - 99-rocal.rules
 > Auto swap power profle depending if the power is connected or not.
 >
 > Usage:  
 > 1 - Copy file or edit a loral rule.  
 > 2 - udevadm control --reload-rules  
 > 3 - udevadm trigger --attr-match=subsystem=net  
+
+
+### 2 - .wezterm.lua 
+> Wezterm config file  
+> 
+> Usage:  
+> 1 - Copy file to user directory
