b3b778b72689caa5118049849df99f4208ffb076
Rui Almeida <rui.almeida011@gmail.com>
2025-10-24 23:27:36 +0100

    wip

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,65 +1,34 @@
 local wezterm = require 'wezterm'
 
-local config = {
+return {
   window_background_opacity = 0.9,
   initial_cols = 120,
-
   hide_tab_bar_if_only_one_tab = true,
-  -- integrated_title_buttons_style = "Gnome"
+  adjust_window_size_when_changint_font_size = false,
+
   font = wezterm.font_with_fallback({
     'SauceCode Pro Nerd Font',
     'Source Code Pro',
     'JetBrainsMono Nerd Font',
     'Hack Nerd Font Mono',
   }),
-  font_size = 11
+  font_size = 11,
+
+  keys = {
+    {
+      key = '-',
+      mods = 'CTRL|ALT',
+      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = '+',
+      mods = 'CTRL|ALT',
+      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = 'w',
+      mods = 'CTRL|ALT',
+      action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
+  }
 }
-
-config.keys = {
-  -- This will create a new split and run your default program inside it
-  {
-    key = '-',
-    mods = 'CTRL|ALT',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = '+',
-    mods = 'CTRL|ALT',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'w',
-    mods = 'CTRL|ALT',
-    action = wezterm.action.CloseCurrentPane { confirm = true },
-  },
-}
-
-return config
--- config.keys =
-
-
-
--- return {
---     window_background_opacity = 0.9,
---     initial_cols = 100,
-
---     hide_tab_bar_if_only_one_tab = true,
---     -- integrated_title_buttons_style = "Gnome"
---     font = wezterm.font('Source Code Pro'),
---     font_size = 11,
-
---     keys = {
---       {
---         key = '+',
---         mods = 'SHIT|ALT',
---         action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
---       },
---     },
-
--- }
--- local config = wezterm.config_builder()
--- config.window_background_opacity = 0.90
--- -- config.enable_tab_bar = false
--- config.hide_tab_bar_if_only_one_tab = true
-
--- return config
